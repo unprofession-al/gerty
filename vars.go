3db292ac5adc4b95ad5d82e6c1aa8c8ab9ff3fe6
Daniel Menet <membership@sontags.ch>
2015-11-19 18:40:21 +0100

    changed order for readability

--- vars.go
+++ vars.go
@@ -5,6 +5,42 @@ import (
 	"sort"
 )
 
+type VarList map[string]interface{}
+
+type VarBucket struct {
+	Name string
+	Prio int
+	Vars VarList
+}
+
+type VarCollection []VarBucket
+
+func (v VarCollection) Len() int           { return len(v) }
+func (v VarCollection) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
+func (v VarCollection) Less(i, j int) bool { return v[i].Prio > v[j].Prio }
+
+func (v *VarCollection) Merge(src string) MergedVars {
+	sort.Sort(v)
+	merged := MergedVars{}
+	for _, buck := range *v {
+		for k, v := range buck.Vars {
+			merged.append(k, v, src, buck.Name)
+		}
+	}
+	return merged
+}
+
+func (v *VarCollection) AddOrReplace(n VarBucket) {
+	for i, vars := range *v {
+		if vars.Name == n.Name {
+			(*v)[i] = n
+			return
+		}
+	}
+	*v = append(*v, n)
+	return
+}
+
 type Merged struct {
 	Key           string
 	Value         interface{}
@@ -46,40 +82,4 @@ func (m *MergedVars) append(key string, value interface{}, source string, bucket
 	}
 
 	*m = append(*m, v)
-}
-
-type VarList map[string]interface{}
-
-type VarBucket struct {
-	Name string
-	Prio int
-	Vars VarList
-}
-
-type VarCollection []VarBucket
-
-func (v VarCollection) Len() int           { return len(v) }
-func (v VarCollection) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
-func (v VarCollection) Less(i, j int) bool { return v[i].Prio > v[j].Prio }
-
-func (v *VarCollection) Merge(src string) MergedVars {
-	sort.Sort(v)
-	merged := MergedVars{}
-	for _, buck := range *v {
-		for k, v := range buck.Vars {
-			merged.append(k, v, src, buck.Name)
-		}
-	}
-	return merged
-}
-
-func (v *VarCollection) AddOrReplace(n VarBucket) {
-	for i, vars := range *v {
-		if vars.Name == n.Name {
-			(*v)[i] = n
-			return
-		}
-	}
-	*v = append(*v, n)
-	return
 }
