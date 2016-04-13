0be13159f7bc82797d4fa86201345e022a7d9bfd
Daniel Menet <membership@sontags.ch>
2016-04-13 17:09:04 +0200

    cleanup of tree renderer

--- transformers/routes.go
+++ transformers/routes.go
@@ -2,8 +2,7 @@ package transformers
 
 func init() {
 	routes = routeDefinition{
-		"TreeRenderer": route{p: "/tree/data.json", m: "GET", h: treeRenderer},
-		"TreeIndex":    route{p: "/tree", m: "GET", h: treeIndex},
+		"TreeRenderer": route{p: "/tree", m: "GET", h: treeRenderer},
 
 		"AnsibleRenderer": route{p: "/ansible", m: "GET", h: ansibleRenderer},
 	}
