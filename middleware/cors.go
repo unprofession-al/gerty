3babe8053200a52742cc17ddfadf9579b928c98b
Daniel Menet <membership@sontags.ch>
2016-04-15 15:47:09 +0200

    added some cors headers in case no origin is provided in request headers

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,6 +9,9 @@ func CorsHeaders(next http.Handler) http.Handler {
 	fn := func(res http.ResponseWriter, req *http.Request) {
 		if origin := req.Header.Get("Origin"); origin != "" {
 			res.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			origin := req.Header.Get("Origin")
+			res.Header().Set("Access-Control-Allow-Origin", "*")
 		}
 		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Accept, Authorization")
