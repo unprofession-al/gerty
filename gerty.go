35df2ab6ab7d5c7a79ead94826fe022260f04c70
Daniel Menet <membership@sontags.ch>
2016-05-20 15:08:36 +0200

    made port and address configurable

--- gerty.go
+++ gerty.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
+	"github.com/sontags/env"
 	"github.com/unprofession-al/gerty/api"
 	"github.com/unprofession-al/gerty/entities"
 	mw "github.com/unprofession-al/gerty/middleware"
@@ -15,7 +16,21 @@ import (
 	_ "github.com/unprofession-al/gerty/store/sqlitestore"
 )
 
+type configuration struct {
+	Port    string `json:"port"`
+	Address string `json:"address"`
+}
+
+var config configuration
+
+func init() {
+	env.Var(&config.Port, "PORT", "8008", "Port to bind to")
+	env.Var(&config.Address, "ADDR", "0.0.0.0", "Address to bind to")
+}
+
 func main() {
+	env.Parse("GERTY", false)
+
 	s, err := store.New("sqlitestore", "/tmp/gerty.sqlite3")
 	if err != nil {
 		panic(err)
@@ -41,5 +56,5 @@ func main() {
 		// mw.WebHook,
 	).Then(r)
 
-	log.Fatal(http.ListenAndServe(":8008", chain))
+	log.Fatal(http.ListenAndServe(config.Address+":"+config.Port, chain))
 }
--- middleware/webhook.go
+++ middleware/webhook.go
@@ -1,15 +1,11 @@
 package middleware
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
 func WebHook(next http.Handler) http.Handler {
 	fn := func(res http.ResponseWriter, req *http.Request) {
 		next.ServeHTTP(res, req)
 		username := req.Header.Get(HeaderUserName)
-		fmt.Println(username)
 		res.Header().Set("X-Message", "Username: "+username)
 	}
 
